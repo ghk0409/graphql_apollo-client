02ce2927526b96c9cbb4b79ae6c24f82bff9f6cb
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-01-31 20:32:14 +0900

    프로젝트 기본 구성 및 Apollo Client 생성 테스트
    - 기본 구조틀 잡기
    - apollo client 생성 후 연결된 graphql api서버와 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
+
 function App() {
-    return <div></div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Movies />} />
+                <Route path="/movies/:id" element={<Movie />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/client.js
@@ -0,0 +1,24 @@
+// GraphQL API url을 이용해 구성
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+
+// Apollo Client 생성 (GraphQL API 서버와 연결)
+const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    // 인메모리캐시 전략
+    cache: new InMemoryCache(),
+});
+
+// client가 정상 연결 작동하는지 확인 (hook을 이용, 서버에 만들어둔 API 호출해보기)
+client
+    .query({
+        query: gql`
+            {
+                allMovies {
+                    title
+                }
+            }
+        `,
+    })
+    .then((data) => console.log(data));
+
+export default client;
