20dac59339ca389fb646d6075720299a6fdf2236
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-02-08 23:17:53 +0900

    Apollo Client 활용
    - 각 컴포넌트별 gql
    - useQuery 활용
    - Apollo Dev Tools 활용
    - Apollo Cache

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+    query getMovie($movieId: String!) {
+        movie(id: $movieId) {
+            id
+            title
+            summary
+        }
+    }
+`;
+
 export default function Movie() {
-    return <div>This is Movie Details~!!</div>;
+    const { id } = useParams();
+
+    const { data, loading, error } = useQuery(GET_MOVIE, {
+        variables: {
+            movieId: id,
+        },
+    });
+
+    if (loading) {
+        return <h1>Loading....</h1>;
+    }
+    if (error) {
+        return <h1>Could not fetch... :(</h1>;
+    }
+
+    return <div>{data.movie.title}</div>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,30 +1,63 @@
-import { gql, useApolloClient } from "@apollo/client";
+import { gql, useApolloClient, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// gql 쿼리 작성
+const ALL_MOVIES = gql`
+    query getMovies {
+        allMovies {
+            title
+            id
+        }
+        allTweets {
+            id
+            text
+            author {
+                fullName
+            }
+        }
+    }
+`;
 
 export default function Movies() {
-    const [movies, setMovies] = useState([]);
-    const client = useApolloClient();
+    // useQuery 활용 (선언형)
+    const { data, loading, error } = useQuery(ALL_MOVIES);
 
-    useEffect(() => {
-        client
-            .query({
-                query: gql`
-                    {
-                        allMovies {
-                            title
-                            id
-                        }
-                    }
-                `,
-            })
-            .then((result) => setMovies(result.data.allMovies));
-    }, [client]);
+    if (loading) {
+        return <h1>Loading....</h1>;
+    }
+    if (error) {
+        return <h1>Could not fetch... :(</h1>;
+    }
 
     return (
-        <div>
-            {movies.map((movie) => (
-                <li key={movie.id}>{movie.title}</li>
+        <ul>
+            <h1>Movies</h1>
+            {data.allMovies.map((movie) => (
+                <li key={movie.id}>
+                    <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
             ))}
-        </div>
+        </ul>
     );
+
+    // gql 기본 (명령형)
+    // const [movies, setMovies] = useState([]);
+    // const client = useApolloClient();
+
+    // useEffect(() => {
+    //     client
+    //         .query({
+    //             query: gql``,
+    //         })
+    //         .then((result) => setMovies(result.data.allMovies));
+    // }, [client]);
+
+    // return (
+    //     <div>
+    //         {movies.map((movie) => (
+    //             <li key={movie.id}>{movie.title}</li>
+    //         ))}
+    //     </div>
+    // );
 }
